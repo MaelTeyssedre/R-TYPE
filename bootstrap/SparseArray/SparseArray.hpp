eaf2b330c83e82c9dddcfb03068ce5f0dcd4d833
Maël Teyssèdre <maelteyssedre@gmail.com>
2021-11-02 14:20:37 +0100

    adding final version of sparseArray

--- bootstrap/SparseArray/SparseArray.hpp
+++ bootstrap/SparseArray/SparseArray.hpp
@@ -9,10 +9,13 @@
 #define SPARSEARRAY_HPP_
 
 #include <vector>
+#include <iostream>
+#include <memory>
+#include <type_traits>
 
 template <typename Component>
-
-class SparseArray {
+class SparseArray
+{
     public:
         using value_type = ???;
         using reference_type = value_type &;
@@ -23,36 +26,122 @@ class SparseArray {
         using const_iterator = typename container_t::const_iterator;
 
         explicit SparseArray() = default;
-        explicit SparseArray(SparseArray const &);
-        explicit SparseArray(SparseArray &&) noexcept;
+        explicit SparseArray(SparseArray const &sparceArray)
+            : _data(sparseArray._data) {}
+        explicit SparseArray(SparseArray &&sparseArray) noexcept
+            : _data(std::move(sparseArray._data)) {}
 
         ~SparseArray() = default;
 
-        SparseArray &operator=(SparseArray const &);
-        SparseArray &operator=(SparseArray &&) noexcept;
+        SparseArray &operator=(SparseArray const &sparseArray) {
+            std::swap(_data, sparseArray._data);
+            return *this;
+        }
 
-        reference_type operator[](size_t idx);
-        const_reference_type operator[](size_t idx) const;
+        SparseArray &operator=(SparseArray &&sparceArray) noexcept {
+            _data = std::move(sparseArray._data);
+            return *this;
+        }
 
-        iterator begin();
-        const_iterator begin() const;
-        const_iterator cbegin() const;
+        reference_type operator[](size_t idx) {
+            return _data[idx];
+        }
 
-        iterator end();
-        const_iterator end() const;
-        const_iterator cend() const;
+        const_reference_type operator[](size_t idx) const {
+            return _data[idx];
+        }
 
-        size_type size() const;
+        iterator begin() {
+            return _data.begin();
+        }
 
-        reference_type insertAt(size_type pos, Component const &);
-        reference_type insertAt(size_type pos, Component &&);
+        const_iterator begin() const {
+            return _data.begin();
+        }
 
-        // template <class... Params>
-        // reference_type emplaceAt(size_type pos, Params &&...);
+        const_iterator cbegin() const {
+            return _data.cbegin();
+        }
 
-        void erase(size_type pos);
+        iterator end() {
+            return _data.end();
+        }
 
-        size_type getIndex(value_type const &) const;
+        const_iterator end() const {
+            return _data.end();
+        }
+
+        const_iterator cend() const {
+            return _data.cend();
+        }
+
+        size_type size() const {
+            return _data.size();
+        }
+
+        reference_type insertAt(size_type pos, Component const &component) {
+            if (pos > _data.size())
+                throw std::invalid_argument("pos superior to SparseArray's size");
+            if (pos == _data.size()) {
+                _data.push_back(component);
+                return _data.back();
+            }
+            container_t new_container;
+            auto i = 0;
+            for (; i < pos; i++)
+                new_container.push_back(_data[i]);
+            new_container.push_back(component);
+            for (; i < _data.size(); i++)
+                new_container.push_back(_data[i]);
+            _data = new_container;
+            return _data[pos];
+        }
+
+        reference_type insertAt(size_type pos, Component &&component) {
+            if (pos > _data.size())
+                throw std::invalid_argument("pos superior to SparseArray's size");
+            if (pos == _data.size()) {
+                _data.push_back(std::move(component));
+                return _data.back();
+            }
+            container_t new_container;
+            auto i = 0;
+            for (; i < pos; i++)
+                new_container.push_back(_data[i]);
+            new_container.push_back(std::move(component));
+            for (; i < _data.size(); i++)
+                new_container.push_back(_data[i]);
+            _data = new_container;
+            return _data[pos];
+        }
+
+        template <class... Params>
+        reference_type emplaceAt(size_type pos, Params &&...params) {
+            if (pos >= _data.size())
+                throw std::invalid_argument("pos superior to SparseArray's size");
+            auto alloc = _data.get_allocator();
+            std::allocator_traits<decltype(alloc)>::destroy(alloc, std::addressof(_data[pos]));
+            std::allocator_traits<decltype(alloc)>::construct(alloc, std::addressof(_data[pos]), std::forward(params)...);
+            return _data[pos];
+        }
+
+        void erase(size_type pos) {
+            container_t new_container;
+            if (pos >= _data.size())
+                throw std::invalid_argument("pos superior to SparseArray's size");
+            for (auto i = 0; i < _data.size(); i++)
+                if (pos != i)
+                    new_container.push_back(_data[i]);
+            _data = new_container();
+        }
+
+        size_type getIndex(value_type const &value) const {
+            auto adresse = std::addressof(value);
+            for (auto i = 0; i < _data.size(); i++)
+                if (adresse == std::addressof(_data[i]))
+                    return i;
+            throw std::invalid_argument("value not found")
+        }
 
     private:
         container_t _data;
