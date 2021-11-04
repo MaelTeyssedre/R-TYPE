af8ec04955e9d0d67a025f82908d0f6905f456a6
unknown <jules.savang@epitech.eu>
2021-11-04 20:42:01 +0100

    return to root

---
+++ cmake-build-release/CMakeFiles/rtype_server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rtype_server.dir/Server/main.cpp.obj"
+  "bin/rtype_server.exe"
+  "bin/rtype_server.exe.manifest"
+  "bin/rtype_server.pdb"
+  "lib/rtype_server.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rtype_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
