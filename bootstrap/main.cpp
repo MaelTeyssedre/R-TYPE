ef1836820fa08f9e22e0eacd993dcb3a6247231c
Maël Teyssèdre <maelteyssedre@gmail.com>
2021-11-07 20:31:06 +0100

    updating main

--- bootstrap/main.cpp
+++ bootstrap/main.cpp
@@ -1,52 +1,35 @@
 #include <SFML/Window.hpp>
 #include "Registry.hpp"
 #include "components.hpp"
+#include "systems.hpp"
 
 int main() {
-    sf::Window window(sf::VideoMode(800, 600), "My Window");
-
-    Registry registry(10);
-
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My Window");
+    Registry registry(1);
     component::position_s pos = {50, 50};
-    std::function<void(Registry &, Entity const &)> PosCreator = [](Registry &reg, Entity const &e){
-        SparseArray<component::position_s> array = reg.getComponents<component::position_s>();
-        array.insertAt(array.size(), std::nullopt);
-    };
-    std::function<void(Registry &, Entity const &)> PosDestructor = [](Registry &reg, Entity const &e){
-        SparseArray<component::position_s> array = reg.getComponents<component::position_s>();
-        array.insertAt(e, std::nullopt);
-    };
-
     component::velocity_s vel = {10, 10};
-    std::function<void(Registry &, Entity const &)> VelCreator = [](Registry &reg, Entity const &e){
-        SparseArray<component::velocity_s> array = reg.getComponents<component::velocity_s>();
-        array.insertAt(array.size(), std::nullopt);
-    };
-    std::function<void(Registry &, Entity const &)> VelDestructor = [](Registry &reg, Entity const &e){
-        SparseArray<component::velocity_s> array = reg.getComponents<component::velocity_s>();
-        array.insertAt(e, std::nullopt);
-    };
-
     component::drawable_s sprite;
     sprite.texture.loadFromFile("canard.png");
     sprite.sprite.setTexture(sprite.texture, true);
-    std::function<void(Registry &, Entity const &)> SpriteCreator = [](Registry &reg, Entity const &e){
-        SparseArray<component::drawable_s> array = reg.getComponents<component::drawable_s>();
-        array.insertAt(array.size(), std::nullopt);
-    };
-    std::function<void(Registry &, Entity const &)> SpriteDestructor = [](Registry &reg, Entity const &e){
-        SparseArray<component::drawable_s> array = reg.getComponents<component::drawable_s>();
-        array.insertAt(e, std::nullopt);
-    };
-    SparseArray<component::position_s> pos_component = registry.registerComponent<component::position_s>(PosCreator, PosDestructor);
-    SparseArray<component::velocity_s> vel_component = registry.registerComponent<component::velocity_s>(VelCreator, VelDestructor);
-    SparseArray<component::drawable_s> draw_component = registry.registerComponent<component::drawable_s>(SpriteCreator, SpriteDestructor);
-    
+    sprite.window = &window;
+    component::controllable_s ctrl = {};
+    registry.registerComponent<component::position_s>(std::function([&](Registry &, Entity const &){}), std::function([&](Registry &, Entity const &){}));
+    registry.registerComponent<component::velocity_s>(std::function([&](Registry &, Entity const &){}), std::function([&](Registry &, Entity const &){}));
+    registry.registerComponent<component::drawable_s>(std::function([&](Registry &, Entity const &){}), std::function([&](Registry &, Entity const &){}));
+    registry.registerComponent<component::controllable_s>(std::function([&](Registry &, Entity const &){}), std::function([&](Registry &, Entity const &){}));
+    registry.addComponent<component::position_s>(registry.entityFromIndex(0), std::move(pos));
+    registry.addComponent<component::velocity_s>(registry.entityFromIndex(0), std::move(vel));
+    registry.addComponent<component::controllable_s>(registry.entityFromIndex(0), std::move(ctrl));
+    registry.addComponent<component::drawable_s>(registry.entityFromIndex(0), std::move(sprite));
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event))
             if (event.type == sf::Event::Closed)
                 window.close();
+        window.clear(sf::Color::Yellow);
+        logging_system(registry);
+        draw_system(registry);
+        window.display();
     }
     return 0;
 }
