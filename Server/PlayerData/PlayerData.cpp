100e0cccad368929bdcf0046f34d30b1a0aec026
MT <maelteyssedre@gmail.com>
2021-11-09 15:35:36 +0100

    Update PlayerData.cpp

--- Server/PlayerData/PlayerData.cpp
+++ Server/PlayerData/PlayerData.cpp
@@ -9,7 +9,7 @@
 #include "PlayerData.hpp"
 
 PlayerData::PlayerData(size_t id, std::shared_ptr<Buffer> buffIn, std::shared_ptr<Buffer> buffOut, std::shared_ptr<std::mutex> mutexIn,std::shared_ptr<std::mutex> mutexOut) 
-: _id(id), _mutexIn(mutexIn), _mutexOut(mutexOut), _bufferIn(buffIn), _bufferOut(buffOut) {}
+    : _id(id), _mutexIn(mutexIn), _mutexOut(mutexOut), _bufferIn(buffIn), _bufferOut(buffOut) {}
 
 size_t PlayerData::getId(void) const
 {
