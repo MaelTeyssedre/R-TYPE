ae8bd4ceb944462736c91da8fe34f981b96444b9
Romain-GABET <romain.gabet@epitech.eu>
2021-11-13 17:19:51 +0100

    indent

--- Server/Network/UDP/udpSocket.hpp
+++ Server/Network/UDP/udpSocket.hpp
@@ -6,25 +6,25 @@
 */
 
 #ifndef UDPSOCKET_HPP_
-#define UDPSOCKET_HPP_
+    #define UDPSOCKET_HPP_
 
-#include "IUDPSocket.hpp"
-#include "Packet.hpp"
-#include <asio.hpp>
+    #include "IUDPSocket.hpp"
+    #include "Packet.hpp"
+    #include <asio.hpp>
 
-class UDPSocket : public IUDPSocket{
-    public:
-        UDPSocket();
-        UDPSocket(asio::io_context &context, std::uint16_t port)  : _context(context), _socket(context, asio::ip::udp::endpoint(asio::ip::udp::v4(), port)) {};
-        ~UDPSocket() = default;
-        void send(IPacket &data) override;
-        std::vector<uint8_t> &receive() override;
-        void handleSend();
+    class UDPSocket : public IUDPSocket{
+        public:
+            UDPSocket();
+            UDPSocket(asio::io_context &context, std::uint16_t port)  : _context(context), _socket(context, asio::ip::udp::endpoint(asio::ip::udp::v4(), port)) {};
+            ~UDPSocket() = default;
+            void send(IPacket &data) override;
+            std::vector<uint8_t> &receive() override;
+            void handleSend();
 
-    private:
-        asio::ip::udp::socket _socket;
-        asio::io_context &_context;
-        asio::ip::udp::endpoint _endpoint;
-};
+        private:
+            asio::ip::udp::socket _socket;
+            asio::io_context &_context;
+            asio::ip::udp::endpoint _endpoint;
+    };
 
 #endif /* !UDPSOCKET_HPP_ */
