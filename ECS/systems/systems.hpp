c08482465232fe16765ea1b275a7ef491a0a2850
MT <maelteyssedre@gmail.com>
2021-11-13 17:43:44 +0100

    Update systems.hpp

--- ECS/systems/systems.hpp
+++ ECS/systems/systems.hpp
@@ -7,8 +7,8 @@
 #ifndef SYSTEMS_HPP_
     #define SYSTEMS_HPP_
 
-#include "Registry.hpp"
-#include "components.hpp"
+    #include "Registry.hpp"
+    #include "components.hpp"
 
     /**
      * \fn void loggingSystem(Registry &r, SparseArray<component::position_s> &positions, SparseArray<component::velocity_s> &velocities)
