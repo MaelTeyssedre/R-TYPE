d6ca3d5dfe1ea9912499dbb1a96b64595f49119d
Romain-GABET <romain.gabet@epitech.eu>
2021-11-13 17:22:35 +0100

    indent

--- Server/Network/User/tcpUser.hpp
+++ Server/Network/User/tcpUser.hpp
@@ -6,33 +6,31 @@
 */
 
 #ifndef TCPUSER_HPP_
-#define TCPUSER_HPP_
+    #define TCPUSER_HPP_
 
-#include <asio.hpp>
-#include <queue>
+    #include <asio.hpp>
+    #include <queue>
 
-class tcpUser : public std::enable_shared_from_this<tcpUser>
-{
-    public: 
-        tcpUser() = default;
-        tcpUser(asio::ip::tcp::socket &&socket);
-        ~tcpUser() = default;
-        void start();
-        void addToQueue(std::vector<uint8_t> message);
-        void read();
-        void doRead(const std::error_code &ec, size_t bytes);
-        void write();
-        void doWrite(const std::error_code &ec);
+    class tcpUser : public std::enable_shared_from_this<tcpUser>
+    {
+        public: 
+            tcpUser() = default;
+            tcpUser(asio::ip::tcp::socket &&socket);
+            ~tcpUser() = default;
+            void start();
+            void addToQueue(std::vector<uint8_t> message);
+            void read();
+            void doRead(const std::error_code &ec, size_t bytes);
+            void write();
+            void doWrite(const std::error_code &ec);
 
-    private:
-        asio::ip::tcp::socket _socket;
-        enum { max_length = 1024}; /** max lenght of one packet */
-        char _data[max_length]; /** placeholders for packet */
-        std::vector<uint8_t> _message; /** message parsed received  from the client */
-        asio::streambuf _input;  /**  raw data read from the client */
-        std::queue<std::vector<uint8_t>> _queue; /** datas to send */
-};
-
-
+        private:
+            asio::ip::tcp::socket _socket;
+            enum { max_length = 1024}; /** max lenght of one packet */
+            char _data[max_length]; /** placeholders for packet */
+            std::vector<uint8_t> _message; /** message parsed received  from the client */
+            asio::streambuf _input;  /**  raw data read from the client */
+            std::queue<std::vector<uint8_t>> _queue; /** datas to send */
+    };
 
 #endif /* !TCPUSER_HPP_ */
