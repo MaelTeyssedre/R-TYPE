79d576d6c93bb30a9aaabf380e1d9a28b556c0b0
Romain-GABET <romain.gabet@epitech.eu>
2021-11-14 16:26:28 +0100

    change protocol

--- Server/PacketManager.cpp
+++ Server/PacketManager.cpp
@@ -16,9 +16,9 @@ PacketManager::PacketManager(std::shared_ptr<Buffer> bufferIn,  std::shared_ptr<
 void PacketManager::createRoom(std::pair<size_t, std::vector<uint8_t>> packet)
 {
     std::vector<uint8_t> vec;
-    std::string str = "Create<";
+    std::string str = "Create ";
     str.append(std::to_string(packet.first));
-    str.append(">;");
+    str.append(";");
     vec.assign(str.begin(), str.end());
     _bufferOut->putInBuffer(vec.size(), vec);
 }
@@ -26,9 +26,9 @@ void PacketManager::createRoom(std::pair<size_t, std::vector<uint8_t>> packet)
 void PacketManager::joinRoom(std::pair<size_t, std::vector<uint8_t>> packet)
 {
     std::vector<uint8_t> vec;
-    std::string  str = "Join<";
+    std::string  str = "Join ";
     str.append(std::to_string(packet.first));
-    str.append(">;");
+    str.append(";");
     vec.assign(str.begin(), str.end());
     _bufferOut->putInBuffer(vec.size(), vec);
 }
