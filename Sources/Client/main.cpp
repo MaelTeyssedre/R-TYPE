0f5fe66e7713ccc7bee58fdd952c324cf7def73b
Romain-GABET <romain.gabet@epitech.eu>
2021-11-17 04:16:15 +0100

    add finish tcp connection

--- Sources/Client/main.cpp
+++ Sources/Client/main.cpp
@@ -9,121 +9,51 @@
 #include <cstring>
 #include <iostream>
 #include "asio.hpp"
-#include <SFML/Window.hpp>
-#include "Registry.hpp"
-#include "components.hpp"
-#include "systems.hpp"
+#include "Packet.hpp"
+#include "TCPClient.hpp"
+#include "NetworkManager.hpp"
 
 using asio::ip::tcp;
 
 enum { max_length = 1024 };
-/*
+
 int main(int argc, char* argv[])
 {
- /* try
-  {
-    if (argc != 3)
-    {
-      std::cerr << "Usage: blocking_tcp_echo_client <host> <port>\n";
-      return 1;
-    }
+  NetworkManager net;
 
-    asio::io_context io_context;
+  std::cout << "before create tcp client" << std::endl;
 
-    tcp::socket s(io_context);
-    tcp::resolver resolver(io_context);
-    asio::connect(s, resolver.resolve(argv[1], argv[2]));
-  
-    /*std::cout << "Enter message: ";
-    char request[max_length];
-    std::cin.getline(request, max_length);
-    size_t request_length = std::strlen(request);
-    asio::write(s, asio::buffer(request, request_length));*/
+  ITCPClient *client =  net.createTCPClient(1332);
+  std::cout << "after create tcp client" << std::endl;
 
-   /* char reply[max_length];
-    size_t reply_length = asio::read(s,
-        asio::buffer(reply, 3));
-    std::cout << "Reply is: ";
-    std::cout.write(reply, reply_length);
-    std::cout << "\n";
+
+  std::string str = "hello from server";
+  std::vector<uint8_t> vec;
+
+  Packet packet;
+
+  vec.assign(str.begin(), str.end());
+  packet.pack(vec);
+  std::cout << "before loop" << std::endl;
+  while (1) {
+    client->receive();
+    client->send(packet);
   }
-  catch (std::exception& e)
-  {
-    std::cerr << "Exception: " << e.what() << "\n";
+ /* asio::io_context context;
+  auto sock = std::make_shared<asio::ip::tcp::socket>(context);
+
+  TCPClient client(context, sock, "127.0.0.1", "1342");
+  context.run();
+  Packet packet;
+
+  std::string str = "hello from server";
+  std::vector<uint8_t> vec;
+
+  vec.assign(str.begin(), str.end());
+  packet.pack(vec);
+  while (1) {
+    client.receive();
+    client.send(packet);
   }*/
-/*
   return 0;
-}*/
-
-void callback_back()
-{
-    std::cout << "Click on Background" << std::endl;
-}
-
-int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My Window");
-    // * Registry
-    Registry registry(0);
-    // * Components creation
-    component::position_s pos {50, 50};
-    component::velocity_s vel {1, 1};
-    component::drawable_s sprite;
-    sprite.texture.loadFromFile("mike.png");
-    sprite.sprite.setTexture(sprite.texture, true);
-    sprite.window = &window;
-    sprite.sprite.setPosition(sf::Vector2f((float)pos.x, (float)pos.y));
-    sprite.sprite.setScale((float)0.3, (float)0.3);
-    component::controllable_s ctrl {true};
-    component::size_s size {800, 600};
-    component::window_s win {&window};
-    
-    component::position_s pos_back {0, 0};
-    component::drawable_s sprite_back;
-    sprite_back.texture.loadFromFile("mike.png");
-    sprite_back.sprite.setTexture(sprite_back.texture, true);
-    sprite_back.window = &window;
-    sprite_back.sprite.setPosition(sf::Vector2f((float)pos_back.x, (float)pos_back.y));
-    sprite_back.sprite.setScale((float)window.getSize().x / sprite_back.texture.getSize().x, (float)window.getSize().y / sprite_back.texture.getSize().y);
-    component::controllable_s ctrl_back {false};
-    //component::callback_s callback_back;
-    //callback_back.Callback = &callback_back;
-
-
-    size_t id_back = registry.spawnEntity();
-    size_t id = registry.spawnEntity();
-
-    // * RegisterComponent
-    registry.registerComponent<component::position_s>([](Registry &, Entity const &) -> void {}, [](Registry &, Entity const &) -> void {});
-    registry.registerComponent<component::velocity_s>([](Registry &, Entity const &) -> void {}, [](Registry &, Entity const &) -> void {});
-    registry.registerComponent<component::drawable_s>([](Registry &, Entity const &) -> void {}, [](Registry &, Entity const &) -> void {});
-    registry.registerComponent<component::controllable_s>([](Registry &, Entity const &) -> void {}, [](Registry &, Entity const &) -> void {});
-    registry.registerComponent<component::size_s>([](Registry &, Entity const &) -> void {}, [](Registry &, Entity const &) -> void {});
-    // * AddComponent
-    registry.addComponent<component::position_s>(registry.entityFromIndex(id), std::move(pos));
-    registry.addComponent<component::velocity_s>(registry.entityFromIndex(id), std::move(vel));
-    registry.addComponent<component::controllable_s>(registry.entityFromIndex(id), std::move(ctrl));
-    registry.addComponent<component::drawable_s>(registry.entityFromIndex(id), std::move(sprite));
-    registry.addComponent<component::size_s>(registry.entityFromIndex(id), std::move(size));
-    //registry.addComponent<component::window_s>(registry.entityFromIndex(id), std::move(win));
-
-    registry.addComponent<component::position_s>(registry.entityFromIndex(id_back), std::move(pos_back));
-    registry.addComponent<component::controllable_s>(registry.entityFromIndex(id_back), std::move(ctrl_back));
-    registry.addComponent<component::drawable_s>(registry.entityFromIndex(id_back), std::move(sprite_back));
-    // * addSystem
-    registry.addSystem(loggingSystem, registry.getComponents<component::position_s>(), registry.getComponents<component::velocity_s>());
-    registry.addSystem(positionSystem, registry.getComponents<component::position_s>(), registry.getComponents<component::drawable_s>());
-    registry.addSystem(controlSystem, registry.getComponents<component::position_s>(), registry.getComponents<component::velocity_s>(), registry.getComponents<component::controllable_s>());
-    registry.addSystem(drawSystem, registry.getComponents<component::drawable_s>());
-    registry.addSystem(clickSystem, registry.getComponents<component::position_s>(), registry.getComponents<component::size_s>()/*, registry.getComponents<component::window_s>()*/);
-    // * mainLoop
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event))
-            if (event.type == sf::Event::Closed)
-                window.close();
-        window.clear(sf::Color::Black);
-        registry.run_system();
-        window.display();
-    }
-    return 0;
 }
