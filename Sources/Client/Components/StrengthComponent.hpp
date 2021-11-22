ccfe623cde1227fc9b54df04e5b39a47a4b4bfee
Alban Bodenes <alban_bodenes@pop-os.localdomain>
2021-11-22 18:29:49 +0100

    Add strength component

---
+++ Sources/Client/Components/StrengthComponent.hpp
@@ -0,0 +1,13 @@
+
+#ifndef STRENGTHCOMPONENT_HPP_
+    #define STRENGTHCOMPONENT_HPP_
+
+    #include <iostream>
+
+    namespace components {
+        struct strength_s {
+            size_t strength;
+        };
+    }
+
+#endif /* !STRENGTHCOMPONENT_HPP_ */
