f393ffc52cb0cc724c17365214f11be2de83afb7
Alban Bodenes <alban_bodenes@pop-os.localdomain>
2021-11-22 18:05:14 +0100

    Add component

---
+++ Sources/Client/Components/SizeComponent.hpp
@@ -0,0 +1,13 @@
+#ifndef SIZECOMPONENT_HPP_
+    #define SIZECOMPONENT_HPP_
+
+    #include <iostream>
+
+    namespace components {
+        struct size_s {
+            size_t x;
+            size_t y;
+        };
+    }
+
+#endif /* !SIZECOMPONENT_HPP_ */
