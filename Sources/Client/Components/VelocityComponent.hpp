08813e608c7ee00c3c54557996251e750a7c96a2
Alban Bodenes <alban_bodenes@pop-os.localdomain>
2021-11-22 16:21:34 +0100

    Add component

---
+++ Sources/Client/Components/VelocityComponent.hpp
@@ -0,0 +1,13 @@
+#ifndef VELOCITYCOMPONENT_HPP_
+    #define VELOCITYCOMPONENT_HPP_
+
+    #include <iostream>
+
+    namespace components {
+        struct velocity_s {
+            size_t x;
+            size_t y;
+        };
+    }
+
+#endif /* !VELOCITYCOMPONENT_HPP_ */
