33072ecd26c71d940390db4afca9782365de80c0
Alban Bodenes <alban_bodenes@pop-os.localdomain>
2021-11-22 16:17:12 +0100

    Add position

---
+++ Sources/Client/Components/PositionComponent.hpp
@@ -0,0 +1,13 @@
+#ifndef POSITIONCOMPONENT_HPP_
+    #define POSITIONCOMPONENT_HPP_
+
+    #include <iostream>
+
+    namespace components {
+        struct position_s {
+            size_t x;
+            size_t y;
+        };
+    }
+
+#endif /* !POSITIONCOMPONENT_HPP_ */
