597ec572e77e04aa07f6f3f719ea4917483895db
Romain-GABET <62942880+Romain-GABET@users.noreply.github.com>
2021-11-23 16:25:54 +0100

    Update Constants.hpp

--- Sources/Client/Constants.hpp
+++ Sources/Client/Constants.hpp
@@ -66,7 +66,9 @@
             enum ENTITY_TYPE {
                 PLAYER,
                 MONSTER,
-                BOSS
+                BOSS,
+                WALL,
+                PROJECTILE
             };
         }
     }
