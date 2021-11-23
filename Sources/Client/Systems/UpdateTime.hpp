61a05ad16b9ef8e09c1ed15ba68441ef4a9d5585
Jsavang <63448421+Jsavang@users.noreply.github.com>
2021-11-23 21:41:37 +0100

    add doc for UpdateTime

--- Sources/Client/Systems/UpdateTime.hpp
+++ Sources/Client/Systems/UpdateTime.hpp
@@ -1,3 +1,9 @@
+/**
+ * \file UpdateTime.hpp
+ *
+ * \brief file where the update time system is defined
+ */
+
 #ifndef UPDATETIME_HPP_
     #define UPDATETIME_HPP_
 
@@ -6,13 +12,35 @@
     #include "Constants.hpp"
     #include "TimeComponent.hpp"
 
+    /**
+     * \class UpdateTime UpdateTime.hpp 
+     * 
+     * \brief class that contain the system UpdateTime
+     */
     class UpdateTime {
         public:
+            /**
+             * \fn explicit UpdateTime() = default
+             *
+             * \brief ctor UpdateTime
+             */
             explicit UpdateTime() = default;
+
+            /**
+             * \fn virtual ~UpdateTime() = default
+             *
+             * \brief dtor UpdateTime
+             */
             virtual ~UpdateTime() = default;
+
+            /**
+             * \fn void operator()(Registry &r, SparseArray<components::time_s> &times)
+             * 
+             * \brief overload of the operator () to be usable by the ECS
+             */
             void operator()(Registry &r, SparseArray<components::time_s> &times);
         private:
-            std::chrono::nanoseconds _currentDeltaTime;
+            std::chrono::nanoseconds _currentDeltaTime; /*! The current delta time*/
     };
 
 #endif /* !UPDATETIME_HPP_ */
