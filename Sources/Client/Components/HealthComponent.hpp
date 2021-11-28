f3797329ad1388ec2aea47ad6dab72d5e44323d5
Alban-Bodenes <68051453+Alban-Bodenes@users.noreply.github.com>
2021-11-28 21:21:38 +0100

    Update HealthComponent.hpp

--- Sources/Client/Components/HealthComponent.hpp
+++ Sources/Client/Components/HealthComponent.hpp
@@ -10,12 +10,6 @@
     #include <iostream>
 
     namespace rtype {
-        /**
-         * \namespace components
-         * 
-         * \brief namespace for ECS components
-         * 
-         */
         namespace components {
             struct health_s {
                 size_t health;
