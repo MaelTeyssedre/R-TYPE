85bbea4e37d0d2eb057dbad3975f641660c57b93
Alban-Bodenes <68051453+Alban-Bodenes@users.noreply.github.com>
2021-11-28 22:02:26 +0100

    Update UpdateTime.cpp

--- Sources/Server/Systems/UpdateTime/UpdateTime.cpp
+++ Sources/Server/Systems/UpdateTime/UpdateTime.cpp
@@ -4,9 +4,6 @@ void rtype::UpdateTime::operator()(Registry &r, SparseArray<components::myTime_s
     (void)r;
     static std::chrono::steady_clock::time_point lastTick = std::chrono::steady_clock::now();
     std::chrono::steady_clock::time_point currentTick = std::chrono::steady_clock::now();
-    // for (auto &&[time] : Zipper(times)) {
-    //     time.deltaTime = (currentTick - lastTick);
-    // }
     std::optional<components::myTime_s> &time = times[rtype::constants::RESERVED_ID::TIME_UPDATE];
     if (time)
         time.value().deltaTime = (currentTick - lastTick);
